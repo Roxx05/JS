fde8618f5c795f5695f3f4e728656cfea89b03a3
Ravi Kumar <raviku.4129@gmail.com>
2025-09-28 17:18:57 +0000

    Add initial data types demonstration in 02_datatypes.js

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // Treat all JS code as newer versions
+// alert(3); // we don't use alert, bczwe are using nodejs not browser
+
+console.log(3+3)
+console.log("Ravi Kumar")
+
+let name = "Ravi"
+let age = 21
+let isLoggedIn = false
+let state; //undefined
+
+//number => 2 to power 53
+//bigint
+//string => '', "", ``
+//boolean => true/false
+//null => standalone value
+//undefined => variable without value
+//symbol => unique
+//object => key value pairs
+
+console.log(typeof null) //bug in js => object
+console.log(typeof undefined); //undefined
