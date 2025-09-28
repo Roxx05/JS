776222ea50e1cc5b9bcb61bf8ee5d6a2cc00163f
Ravi Kumar <raviku.4129@gmail.com>
2025-09-28 16:06:06 +0000

    Refactor account variable assignments and update console output

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -2,7 +2,12 @@ const accountId = 240721
 let accountEmail ="ravi@google.com"
 var accountPassword = "12345"
 accountCity = "Rewari"
+ let accountState;
 
-// accountId = 3
+// accountId = 2 //NOT ALLOWED
+accountEmail="rocky@gy.com"
+accountPassword="4129"
+accountCity="Pune"
 
 console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity, accountState]) 
